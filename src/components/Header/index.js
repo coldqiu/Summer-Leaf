3e92277ba3dafbf170b528ccf080578ab32717f8
coldqiu <gotta_blue@163.com>
2021-07-23 16:10:38 +0800

    修改MHeader组件样式

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,31 +7,37 @@ import Style from "./index.less";
 function MHeader(props) {
   // const bool = useSelector((state) => state.bool);
   console.log("header props", props);
-  // console.log("")
   // console.log("props.location.pathname", props.location.pathname);
   const dispatch = useDispatch();
   return (
     <div className={Style.wrap}>
       <div className={Style.left}>
-        <ul className={Style.leftMenu}>
-          <li className={Style.menuItem}>
-            <span
-              onClick={() => dispatch(ToggleSiderBar())}
-              className={"iconfont icon-add " + Style.copy}
-            ></span>
+        <ul className={Style.ul}>
+          <li className={Style.li}>
+            <div className={Style.icon}>
+              <span
+                onClick={() => dispatch(ToggleSiderBar())}
+                className={"iconfont icon-add " + Style.copy}
+              ></span>
+            </div>
           </li>
         </ul>
       </div>
       <div className={Style.middle}>
-        <h1>{props.title}</h1>
+        {/* <h1>{props.title}</h1> */}
+        <h1>{props.location.pathname}</h1>
       </div>
       <div className={Style.right}>
-        <ul className={Style.rightMenu}>
-          <li className={Style.menuItem}>
-            <span className={"iconfont icon-add " + Style.copy}></span>
+        <ul className={Style.ul}>
+          <li className={Style.li}>
+            <div className={Style.icon}>
+              <span className={"iconfont icon-add " + Style.copy}></span>
+            </div>
           </li>
-          <li className={Style.menuItem}>
-            <span className={"iconfont icon-add " + Style.add}></span>
+          <li className={Style.li}>
+            <div className={Style.icon}>
+              <span className={"iconfont icon-add " + Style.copy}></span>
+            </div>
           </li>
         </ul>
       </div>
