398eb2d1117a31d9d2c9bc14ed3abef3fdf5d102
coldqiu <gotta_blue@163.com>
2021-08-05 20:18:31 +0800

    添加虚拟列表

--- src/base/gird.jsx
+++ src/base/gird.jsx
@@ -1,15 +1,36 @@
 //
 import { useEffect, useCallback, useState } from "react";
+import VirtualList from "react-tiny-virtual-list";
 
 import Style from "./grid.less";
 import List from "./list";
 
 export default function Grid({ list = [], click }) {
   const arr = list.map((item) => <GridItem item={item} key={item.title} click={click} />);
-  return <ul className={Style.ul}>{arr}</ul>;
+  console.log("list.lenght", list.length);
+  const vlist = (
+    <VirtualList
+      width="100%"
+      height={870}
+      itemCount={list.length}
+      itemSize={105}
+      // overscanCount={10}
+      // estimatedItemSize={1}
+      renderItem={({ index, style }) => (
+        // <div key={index} style={style}>
+        //   Letter: {list[index]}, Row: #{index}
+        // </div>
+        <GridItem item={list[index]} key={list[index].title} click={click} style={style}>
+          index:{index}
+        </GridItem>
+      )}
+    ></VirtualList>
+  );
+  // return <ul className={Style.ul}>{arr}</ul>;
+  return <ul className={Style.ul}>{vlist}</ul>;
 }
 
-export function GridItem({ item, click }) {
+export function GridItem({ item, click, style }) {
   const [dom, setDom] = useState(null);
   const ref = useCallback((node) => {
     setDom(node);
@@ -33,10 +54,19 @@ export function GridItem({ item, click }) {
   }, []);
 
   return (
-    <li onClick={() => handleClick(item)} key={item.title} className={Style.li}>
+    <li
+      onClick={() => handleClick(item)}
+      key={item.title}
+      className={Style.li}
+      style={style}
+    >
       {/* // 点击时记录【图片】位置 ，即Detail 动画初始位置 */}
       <img ref={ref} src={item.coverPic1} alt={item.coverPic} />
-      <List info={item.info ? item.info : { title: "default title", left: "xx", right: " yy" }}></List>
+      <List
+        info={
+          item.info ? item.info : { title: "default title", left: "xx", right: " yy" }
+        }
+      ></List>
     </li>
   );
 }
